be1ae77e10861c91ba3c0a7f391208e0bab37d29
Franco S <jonfrancoz@gmail.com>
2021-09-28 13:44:28 -0700

    git commit "initial commit, working on eliminate"

---
+++ function.py
@@ -0,0 +1,28 @@
+from utils import *
+
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+
+    while True:
+        none_deleted = True
+        eliminated_grid = ''
+
+        if none_deleted:
+            break
+
+    return eliminated_grid
+
+
+if __name__ == '__main__':
+    grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+    eliminate(grid_values(grid))
