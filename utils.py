4dcbc17f6d726930c83ef0601b2221e318662469
Franco S <jonfrancoz@gmail.com>
2021-09-29 06:00:25 -0700

    constraint propagation!

--- utils.py
+++ utils.py
@@ -33,27 +33,48 @@ def display(values):
     return
 
 
-def grid_values(grid: str) -> dict:
-    """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
-
-    Args:
-        grid: Sudoku grid in string form, 81 characters long
-    Returns:
-        Sudoku grid in dictionary form:
-        - keys: Box labels, e.g. 'A1'
-        - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
+def grid_values(grid):
     """
-    values = []
-    all_digits = '123456789'
+    Convert grid into a dict of {square: char} with '123456789' for empties.
+    Input: A grid in string form.
+    Output: A grid in dictionary form
+            Keys: The boxes, e.g., 'A1'
+            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
+    """
+    chars = []
+    digits = '123456789'
     for c in grid:
+        if c in digits:
+            chars.append(c)
         if c == '.':
-            values.append(all_digits)
-        elif c in all_digits:
-            values.append(c)
-    assert len(values) == 81
-    return dict(zip(boxes, values))
+            chars.append(digits)
+    assert len(chars) == 81
+    return dict(zip(boxes, chars))
 
 
-if __name__ == '__main__':
-    print(units)
-    print(peers)
+def eliminate(values):
+    """
+    Go through all the boxes, and whenever there is a box with a value, eliminate this value from the values of all its peers.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit, '')
+    return values
+
+
+def only_choice(values):
+    """
+    Go through all the units, and whenever there is a unit with a value that only fits in one box, assign the value to this box.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
