058be1e79a6e4bbfdad24e6927436cd8b78b7d3c
Franco S <jonfrancoz@gmail.com>
2021-10-07 08:26:34 -0700

    finally... complete. fixed a type in diag unit

--- 1_Sudoku/solution.py
+++ 1_Sudoku/solution.py
@@ -6,8 +6,8 @@ row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI')
                 for cs in ('123', '456', '789')]
-diagonal_units = [['ABCEDFGHI'[index]+'123456789'[index]
-                   for index in range(9)], ['ABCEDFGHI'[index]+'987654321'[index] for index in range(9)]]
+diagonal_units = [['ABCDEFGHI'[index]+'123456789'[index]
+                   for index in range(9)], ['IHGFEDCBA'[index]+'123456789'[index] for index in range(9)]]
 
 unitlist = row_units + column_units + square_units + diagonal_units
 
